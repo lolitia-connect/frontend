89d81703bed432ee19662e488ce0765bc7bdb3b2
KawaiiCat <cat@lolitia.fyi>
2026-06-22 20:46:39 +0800

    Remove sponsor info on admin dashboard

--- apps/admin/src/sections/payment/index.tsx
+++ apps/admin/src/sections/payment/index.tsx
@@ -1,13 +1,9 @@
-import Billing from "../dashboard/components/billing";
 import PaymentTable from "./payment-table";
 
 export default function Payment() {
   return (
     <>
       <PaymentTable />
-      <div className="mt-5 flex flex-col gap-3">
-        <Billing type="payment" />
-      </div>
     </>
   );
 }

5dfede099a3feb3f9561c770f339f790e7ae9a72
KawaiiCat <cat@lolitia.fyi>
2026-06-29 15:56:00 +0800

    Less padding on mobile devices

--- apps/user/src/sections/user/layout.tsx
+++ apps/user/src/sections/user/layout.tsx
@@ -12,7 +12,7 @@ export default function UserLayout() {
   return (
     <SidebarProvider className="container">
       <SidebarLeft className="sticky top-[84px] hidden w-52 border-r-0 bg-transparent lg:flex" />
-      <SidebarInset className="relative p-4">
+      <SidebarInset className="relative px-3 py-4 md:p-4">
         <Outlet />
       </SidebarInset>
       <SidebarRight className="sticky top-[84px] hidden w-52 border-r-0 bg-transparent 2xl:flex" />
